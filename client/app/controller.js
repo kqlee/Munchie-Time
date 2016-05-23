1f38f4a15cb18180f7614c364648b15db2d0ef8c
Kent <kentqlee@gmail.com>
2016-05-23 16:41:06 -0700

    Add factory

--- client/app/controller.js
+++ client/app/controller.js
@@ -1,50 +1,56 @@
 angular.module('yelpApp', [])
 
-.controller('searchController', function ($scope, $http) {
+.controller('searchController', function ($scope, searchFactory) {
+  angular.extend($scope, searchFactory);
   $scope.searchBox = 'Find Your Next Meal';
 
-  $scope.data = {};
+})
 
+.controller('resultsController', function ($scope, searchFactory) {
+  angular.extend($scope, searchFactory);
+  $scope.currResults = 'Search Results:';
+
+})
+
+//Factory to hold all share methods across controllers
+.factory('searchFactory', function($http) {
+
+  var results = [];
   var yelpUrl = 'https://api.yelp.com/v2/search';
   var yelpKey = window.consumer_key;
 
   var defaultParams = {
     location: 'San Francisco',
     term: 'Food', //change to different meals later
-    limit: 4,
+    limit: 3,
     sort: 0,
     radius_filter: 8000 //5-mile radius
   };
 
-  $scope.searchYelp = function() {
-    $http({
-      method: 'POST',
-      url: '/yelpSearch',
-      params: defaultParams
-    }).then(function success(data) {
-      console.log(data);
-      $scope.data = data;
+  var searchYelp = function() {
+    $http.post('/', defaultParams)
+    .then(function success(data) {
+      console.log('DATA:', data);
+      addData(data);
+      return data;
     }, function error(err) {
-      console.error(err);
+      console.error('ERROR:', err);
     });
   };
-  
-})
 
-.controller('resultsController', function ($scope) {
-  $scope.results = 'Search Results:';
-  $scope.data = window.examplesearchdata.businesses;
-
-  $scope.addData = function() {
-    window.examplesearchdata.businesses.forEach(function(business, index) {
-      var storage = $scope.data;
-      storage.push(business);
-
-    });  
-    console.log($scope.data);
+  var addData = function(searchQuery) {
+    console.log(query);
+    var query = searchQuery; //|| window.examplesearchdata.businesses;
+    //Change this array to API data later
+    query.forEach(function(business, index) {
+      results.push(business);
+    }); 
   }; 
 
-
-
-
+  //Return an object with shared methods
+  return {
+    results: results,
+    searchYelp: searchYelp,
+    addData: addData
+  };
 });
